a393150d9a6362f420cb442c147151c5274106d0
pjriveraiupui <31328842+pjriveraiupui@users.noreply.github.com>
2017-09-03 15:59:21 -0400

    include fix

---
+++ ex1/_includes/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("ABSOLUTE_PATH", "/home/pjrivera/htdocs/expm1/");
+
+define("URL_ROOT", "http://corsair.cs.iupui.edu:20271/ex1/");
+?>
---
+++ ex1/_includes/footer.inc.php
@@ -0,0 +1,34 @@
+<div id="footer">
+  <div>
+    <ul class="navigation">
+      <li class="active">
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="about.php">About</a>
+      </li>
+      <li>
+        <a href="rooms.php">Rooms</a>
+      </li>
+      <li>
+        <a href="dives.php">Dive Site</a>
+      </li>
+      <li>
+        <a href="foods.php">Food</a>
+      </li>
+      <li>
+        <a href="news.php">News</a>
+      </li>
+      <li>
+        <a href="contact.php">Contact</a>
+      </li>
+    </ul>
+    <div id="connect">
+      <a href="http://pinterest.com/fwtemplates/" target="_blank" class="pinterest"></a> <a href="http://freewebsitetemplates.com/go/facebook/" target="_blank" class="facebook"></a> <a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a> <a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a>
+    </div>
+  </div>
+  <p>
+    © 2023 by BHACCASYONIZTAS BEACH RESORT. All Rights Reserved
+  </p>
+</div>
+</div>
