d27caf0c59bf7f3ee09f0cb667605ee3f7e98b39
pjriveraiupui <31328842+pjriveraiupui@users.noreply.github.com>
2017-10-29 21:00:59 -0400

    ex5

---
+++ ex5/application/config (4:5:15, 9:08 PM).php
@@ -0,0 +1,12 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:21941/ex5/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'pjrivera');
+define('DB_PASS', 'pjrivera');
+define('DB_NAME', 'pjrivera_db');
+
+define('REQFIELD', '<font color="#FF0000">*</font>');
+?>
