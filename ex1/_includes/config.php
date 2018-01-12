36505427fafca46ad56d32df5c8f013dbc497354
pjriveraiupui <31328842+pjriveraiupui@users.noreply.github.com>
2018-01-11 21:24:55 -0500

    f

--- ex1/_includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("ABSOLUTE_PATH", "/home/pjrivera/htdocs/ex1/");
-
-define("URL_ROOT", "http://corsair.cs.iupui.edu:20271/ex1/");
-?>
--- final/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Display errors in production mode
-
-
-// let's get started
-require 'application/router.php';
