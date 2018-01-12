32e389738644fe913d6f6bbe2130750c060514c8
pjriveraiupui <31328842+pjriveraiupui@users.noreply.github.com>
2018-01-11 21:26:22 -0500

    z

--- a3/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Display errors in production mode
-ini_set('display_errors', 1);
-
-// let's get started
-require 'application/router.php';
--- a3/views/home.php
+++
@@ -1,7 +0,0 @@
-<?php include('elements/header.php');?>
-<div class="container">
-	<div class="page-header">
-    <h1> Hello From the View </h1>
-  </div>
-</div>
-<?php include('elements/footer.php');?>
