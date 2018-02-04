8420c44204f56d430c2d6bc5a30aca6299f2aca7
pjriveraiupui <31328842+pjriveraiupui@users.noreply.github.com>
2018-02-04 16:14:17 -0500

    ex2

---
+++ ex2/classes/admin.class.php
@@ -0,0 +1,25 @@
+<?php
+class admin extends users {
+
+
+  public function __construct($user_level, $user_id) {
+    parent::__construct($user_level);
+    $this->user_type = "2";
+    $this->user_id = "admin1";
+  }
+
+  public function __set($name,$value) {
+    $this->$name = $value;
+    return;
+  }
+
+  public function __get($name) {
+    return $this->$name;
+  }
+
+  public function __destruct() {
+
+  }
+}
+
+?>
